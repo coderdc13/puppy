75e35c8f71515fa227ba972caec7ac492c0db8c2
coderdc13 <franklin.rivera.bueno@gmail.com>
2020-12-07 07:13:17 -0500

    Create puppycoincrowdsale.sol
    adding puppycoincrowdsale.sol

---
+++ puppycoincrowdsale.sol
@@ -0,0 +1,47 @@
+pragma solidity ^0.5.5;
+
+import "./PupperCoin.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/Crowdsale.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/emission/MintedCrowdsale.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/validation/CappedCrowdsale.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/validation/TimedCrowdsale.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/crowdsale/distribution/RefundablePostDeliveryCrowdsale.sol";
+
+
+
+contract PupperCoinSale is Crowdsale, MintedCrowdsale, CappedCrowdsale, TimedCrowdsale, RefundablePostDeliveryCrowdsale {
+    
+    constructor( uint rate, address payable wallet, PupperCoin token, uint goal, uint open, uint close) 
+    
+    Crowdsale(rate, wallet, token)
+    
+    CappedCrowdsale(goal)
+    
+    TimedCrowdsale(open, close)
+    
+    RefundableCrowdsale(goal)
+    
+    public {
+        
+    }
+}
+
+contract PupperCoinCrowdSaleDeployer {
+    address public token_sale_address;
+    address public token_address;
+    
+    constructor(string memory name, string memory symbol, address payable wallet, uint goal) public {
+        PupperCoin token = new PupperCoin(name, symbol, 0);
+        token_address = address(token);
+        
+        PupperCoinSale token_sale = new PupperCoinSale(1, wallet, token, goal, now, now + 440 minutes);
+        
+        token_sale_address = address(token_sale);
+        
+        token.addMinter(token_sale_address);
+        token.renounceMinter();
+    }
+}
+
+
+
